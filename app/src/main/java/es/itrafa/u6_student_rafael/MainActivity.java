f757c95233c0a585b427f0a41d23fdba6894a808
it-rafa <it-rafamartinez@outlook.es>
2022-05-11 14:12:41 +0200

    init Android project

---
+++ app/src/main/java/es/itrafa/u6_student_rafael/MainActivity.java
@@ -0,0 +1,14 @@
+package es.itrafa.u6_student_rafael;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
