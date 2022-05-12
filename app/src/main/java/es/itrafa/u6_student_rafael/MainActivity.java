d20f5556c8d829e8eea7c04d9fa252179306adf2
it-rafa <it-rafamartinez@outlook.es>
2022-05-12 19:26:32 +0200

    work but camera

--- app/src/main/java/es/itrafa/u6_student_rafael/MainActivity.java
+++ app/src/main/java/es/itrafa/u6_student_rafael/MainActivity.java
@@ -1,12 +1,18 @@
 package es.itrafa.u6_student_rafael;
 
-import es.itrafa.u6_student_rafael.R;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -14,9 +20,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-import java.util.Objects;
-
 public class MainActivity extends AppCompatActivity {
+    private ActivityResultLauncher<Intent> activityResultLauncher;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +32,23 @@ public class MainActivity extends AppCompatActivity {
         Toolbar myToolBar = findViewById(R.id.myToolBar);
         setSupportActionBar(myToolBar);
 
-        ActivityResultLauncher activityResultLauncher;
-
-        registerForActivityResult(new
-                ActivityResultContracts.StartActivityForResult(), result -> {
+        activityResultLauncher =
+                registerForActivityResult(new
+                        ActivityResultContracts.StartActivityForResult(), result -> {
                     if (result.getResultCode() == RESULT_OK && result.getData() != null) {
-                        Bundle bundle = result.getData().getExtras();
                         Intent data = result.getData();
+                        if (data.hasExtra("IsRandomNumEven")) {
+                            if (data.getExtras().getBoolean("IsRandomNumEven")){
+                                Toast.makeText(getApplicationContext(), getString(R.string.isEvenMsg) , Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(getApplicationContext(), getString(R.string.isOddMsg) , Toast.LENGTH_SHORT).show();
+                            }
 
+                        } else {
+                            Toast.makeText(getApplicationContext(), getString(R.string.msg_fail_check_even), Toast.LENGTH_SHORT).show();
+                        }
                     } else
-                        Toast.makeText(getApplicationContext(), "Saliste de la actividad secundaria sin pulsar el botón Cerrar", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), getString(R.string.no_backbtn_used), Toast.LENGTH_SHORT).show();
                 });
         ManageEventsMainActivity();
     }
@@ -49,28 +62,43 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-        Toast.makeText(getApplicationContext(), "Pulsado elemento: " + item.getTitle().toString(),
-                Toast.LENGTH_SHORT).show();
+
+        if(item.getItemId() == R.id.itemCamera){
+
+        }else if (item.getItemId() == R.id.itemExit){
+            finish();
+
+        }else{
+            Toast.makeText(getApplicationContext(), "raro, raro",
+                    Toast.LENGTH_SHORT).show();
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
-    private void ManageEventsMainActivity(){
+    private void ManageEventsMainActivity() {
         findViewById(R.id.btn_changeActivity).setOnClickListener(
                 view -> {
-                    int randomNum = (int)(Math.random() * 25 + 1);
-                    String n=Integer.toString(randomNum);
+                    int randomNum = (int) (Math.random() * 25 + 1);
+                    String n = Integer.toString(randomNum);
 
-                    TextView txt = (TextView) findViewById(R.id.numTest);
+                    TextView txt = findViewById(R.id.numTest);
                     txt.setText(n);
 
                     Intent intent = new Intent(getApplicationContext(),
                             ShowNumberActivity.class);
 
-                    intent.putExtra("randomNum", n);
+                    intent.putExtra("RandomNum", n);
+                    activityResultLauncher.launch(intent);
+                }
+        );
+        findViewById(R.id.btn_changeActivity).setOnLongClickListener(
+                view -> {
+                    Intent intent = new Intent(Intent.ACTION_SENDTO);
+                    intent.setData(Uri.parse("mailto:" + ""));
+                    startActivity(intent);
 
+                    return true;
                 }
         );
 
--- app/src/main/java/es/itrafa/u6_student_rafael/ShowNumberActivity.java
+++ app/src/main/java/es/itrafa/u6_student_rafael/ShowNumberActivity.java
@@ -1,10 +1,13 @@
 package es.itrafa.u6_student_rafael;
 
+import es.itrafa.u6_student_rafael.R;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class ShowNumberActivity extends AppCompatActivity {
@@ -18,10 +21,35 @@ public class ShowNumberActivity extends AppCompatActivity {
         setSupportActionBar(myToolBar);
 
         Intent intent = getIntent();
-        TextView txtView_showNumber = (TextView) findViewById(R.id.txtView_showNumber);
+        TextView txtView_showNumber = findViewById(R.id.txtView_showNumber);
 
 
         txtView_showNumber.setText(getString(R.string.randomNumMsg) + " "
-                + intent.getExtras().getString("randomNum"));
+                + intent.getExtras().getString("RandomNum"));
+
+        ManageEventsShowNumberActivity();
+    }
+
+    private void ManageEventsShowNumberActivity() {
+        findViewById(R.id.btn_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent returnIntent = new Intent();
+                int num = Integer.parseInt(getIntent().getExtras().getString("RandomNum"));
+                boolean result;
+                if (num % 2 == 0) {
+                    result = true;
+                } else {
+                    result = false;
+                }
+                returnIntent.putExtra("IsRandomNumEven",
+                        result);
+                setResult(RESULT_OK, returnIntent);
+                finish();
+            }
+        });
+
+
     }
 }
